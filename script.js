1cdb1b621fd6e1a91aef09e6f101938eea064f90
Dominik Bąk <dominikbak93@mgail.comgit config --global user.email dominikbak93@gmail.com>
2019-12-17 19:19:24 +0100

    create chartdata function

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 //API 
 
+function  chartdata() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','dane.json',true);
+    xhr.onload = function(){
+                
+        //konwersja tekstu Json na obiekty javaScript
+        responseObject = JSON.parse(xhr.responseText); 
+            
+
+                
+    };
+
+    xhr.send(null);
 
